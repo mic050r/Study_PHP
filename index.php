87f2c59457e8fa7a4e158b152c1d646476d49cab
mic050r <s2216@e-mirim.hs.kr>
2023-03-19 20:35:34 +0900

    8일차 글 삭제

--- index.php
+++ index.php
@@ -49,9 +49,13 @@ function print_list(){
         <!-- id값이 있어야지만 실행됨 
         -> 홈으로 가면 안보이고 각각이 항목에 가면 보임 -->
         <!-- <?php echo $_GET['id'];?> -->
-        <a href="update.php?id=<?=$_GET['id'];?>">Update</a> 
-        
-    <?php }?>
+        <a href="update.php?id=<?=$_GET['id'];?>">update</a> 
+        <!-- <a href="delete_process.php?id=<?=$_GET['id'];?>">delete</a>  -->
+        <form action="delete_process.php" method="post">
+        <input type="hidden" name="id" value="<?=$_GET['id']?>"/>
+        <input type="submit" value="delete"/>
+        </form>
+        <?php }?>
     <h2>
         <?php
         print_title();
