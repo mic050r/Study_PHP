82af6b8728cd4bc629674d8d5356fbf8fdd025c4
mic050r <s2216@e-mirim.hs.kr>
2023-03-25 21:18:15 +0900

    9일차 보안 XSS

--- lib/print.php
+++ lib/print.php
@@ -2,7 +2,7 @@
 // 제목바꾸기
 function print_title(){
     if(isset($_GET['id'])){
-        echo $_GET['id'];
+        echo htmlspecialchars($_GET['id']);
     }else{ //id값이 존재하지 않는다면
         echo "Welcome";
     }
@@ -10,7 +10,7 @@ function print_title(){
 function print_description(){
     if(isset($_GET['id'])){
         //echo data/id 값에 해당하는 파일의 내용;
-    echo file_get_contents("data/".$_GET['id']);
+    echo htmlspecialchars(file_get_contents("data/".$_GET['id']));
     }else{
         echo "Hello, PHP";
     }
@@ -20,8 +20,9 @@ function print_list(){
     $list = scandir('./data');
     $i = 0;
     while($i < count($list)){
+        $title = htmlspecialchars($list[$i]);
         if($list[$i] != '.' && $list[$i] != '..'){
-            echo " <li><a href = \"index.php?id=$list[$i]\"> $list[$i]</a></li>\n";
+            echo " <li><a href = \"index.php?id=$title\"> $title</a></li>\n";
         }
          $i = $i + 1;
     }
