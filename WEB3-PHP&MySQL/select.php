e83c9a49dd3f79e9719e82f78feafc059718ea12
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-27 00:56:01 +0900

    8-3. 반복문을 활용하여 모든 값 불러오기

--- WEB3-PHP&MySQL/select.php
+++ WEB3-PHP&MySQL/select.php
@@ -1,10 +1,21 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "0123456789**", "opentutorials");
-$sql = "SELECT * FROM topic WHERE id = 19";
+
+// row가 하나일때
+echo "<h1>single row</h1>";
+$sql = "SELECT * FROM topic";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result);
-
-print_r($row);
-echo '<h1>'.$row['title'].'</h1>';
+echo '<h2>'.$row['title'].'</h2>';
 echo $row['description'];
+
+// row가 여러개일때
+echo "<h1>multi row</h1>";
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+// null이면 false
+while($row = mysqli_fetch_array($result)) {
+  echo '<h2>'.$row['title'].'</h2>';
+  echo $row['description'];
+}
 ?>
