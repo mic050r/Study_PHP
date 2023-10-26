26f07002c858f42221bcd79ca41357ebb2470f1d
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-27 00:00:41 +0900

    6-2. mysqli_query

--- WEB3-PHP&MySQL/insert.php
+++ WEB3-PHP&MySQL/insert.php
@@ -1,3 +1,14 @@
 <?php
-mysqli_connect("localhost", "root", "0123456789**", "opentutorials");
+$conn = mysqli_connect("localhost", "root", "0123456789**", "opentutorials");
+
+mysqli_query($conn, "
+    INSERT INTO topic (
+        title,
+        description,
+        created
+    ) VALUES (
+        'MySQL',
+        'MySQL is ....',
+        NOW()
+    )");
 ?>
