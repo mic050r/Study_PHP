4cdad9b875453efc20f70a851c258fa29369c922
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-27 00:38:51 +0900

    feat : 제출 버튼을 누를 때 제목과 설명 필드에 값이 없으면 프롬프트가 나타나도록

--- WEB3-PHP&MySQL/create.php
+++ WEB3-PHP&MySQL/create.php
@@ -9,10 +9,25 @@
     <ol>
       <li>HTML</li>
     </ol>
-    <form action="process_create.php" method="POST">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p><textarea name="description" placeholder="description" cols="30" rows="10"></textarea></p>
-      <p><input type="submit"></p>
+    <form action="process_create.php" method="POST" onsubmit="return validateForm()">
+      <p><input type="text" name="title" id="title" placeholder="title"></p>
+      <p><textarea name="description" id="description" placeholder="description" cols="30" rows="10"></textarea></p>
+      <p><input type="submit" value="Submit"></p>
     </form>
+
+    <script>
+    function validateForm() {
+      var title = document.getElementById("title").value;
+      var description = document.getElementById("description").value;
+
+      if (title === "" || description === "") {
+        alert("제목과 설명을 모두 입력해주세요.");
+        return false; // 제출을 중단
+      }
+
+      return true;
+    }
+    </script>
+
   </body>
 </html>
