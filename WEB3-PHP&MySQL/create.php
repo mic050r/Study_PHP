a2890d406a7e64302b6e7b22e2ffd05d7e5884c3
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-29 05:43:26 +0900

    9-2. 목록에 따라 그에 맞는 본문 출력하기

--- WEB3-PHP&MySQL/create.php
+++ WEB3-PHP&MySQL/create.php
@@ -1,3 +1,30 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "0123456789**", "opentutorials");
+
+$sql = "SELECT * FROM topic";
+$result = mysqli_query($conn, $sql);
+$list = '';
+while($row = mysqli_fetch_array($result)){
+  // <li><a href=\"index.php?id=19\">MySQL</a></li>
+  $list =  $list."<li><a href=\"index.php?id={$row['id']}\">{$row['title']}</a></li>";
+}
+
+// 기본값 셋팅
+$article = array(
+    'title' => 'Welcome',
+    'description' => 'Hello, web'
+  );
+if (isset($_GET["id"])) {
+  $sql = "SELECT * FROM topic WHERE id={$_GET['id']}";
+  $result = mysqli_query($conn, $sql);
+  $row = mysqli_fetch_array($result);
+  $article['title'] =  $row['title'];
+  $article['description'] = $row['description'];
+   
+}
+// print_r($article);
+?>
+
 <!doctype html>
 <html>
   <head>
@@ -5,10 +32,15 @@
     <title>WEB</title>
   </head>
   <body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a> </h1>
     <ol>
-      <li>HTML</li>
+      <?=$list?>
     </ol>
+    <a href="create.php">create</a>
+    <h2><?=$article['title']?></h2>
+    <?=$article['description']?>
+  </body>
+</html>
     <form action="process_create.php" method="POST" onsubmit="return validateForm()">
       <p><input type="text" name="title" id="title" placeholder="title"></p>
       <p><textarea name="description" id="description" placeholder="description" cols="30" rows="10"></textarea></p>
