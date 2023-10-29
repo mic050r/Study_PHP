cd75dee504dd2e507ccfaafce208419b242202b0
mic050r <103114387+mic050r@users.noreply.github.com>
2023-10-30 01:31:54 +0900

    [Snack] 삭제 기능 구현 완료

---
+++ Snack/db/delete.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET["id"])) {
+    // 데이터베이스 연결 설정
+  include('./connect.php');
+
+    $id = $_GET["id"];
+
+    // 데이터베이스에서 선택한 항목 삭제
+    $query = "DELETE FROM snack WHERE id=$id";
+    if ($conn->query($query) === TRUE) {
+        // 삭제 성공 시 리다이렉션
+        header('Location: ../index.html');
+    } else {
+        echo "삭제 실패: " . $conn->error;
+    }
+
+    // 데이터베이스 연결 종료
+    $conn->close();
+}
+?>
